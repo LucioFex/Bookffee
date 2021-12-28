b2073a258b757c341a81863ec36795c8e49e60f5
LucioFex <luciano1d2esteban@gmail.com>
2021-12-28 17:20:14 -0300

    feat: Complete change of the logic in the getBooksData func

--- helpers/booksApi.js
+++ helpers/booksApi.js
@@ -1,39 +1,18 @@
-"use strict"
-const https = require("https");
+"use strict";
 
-let options = {
-    host: 'www.googleapis.com',
-    path: `www.googleapis.com/books/v1/volumes?q=harry+potter:keyes&key=${process.env.apiKey}`,
-    headers: {'User-Agent': 'request'}
-}
+const fetch = require("node-fetch");
 
-const getBooksData = () => {
-    https.get(options, (res) => {
-        let json = "";
 
-        // Received chunk of data
-        res.on('data', (chunk) => {json += chunk});
+const getBooksData = async () => {
+    const url = `https://www.googleapis.com/books/v1/volumes?q=harry+potter:keyes&key=${process.env.apiKey}`;
+    const response = await fetch(url);
+    const json = await response.json();
 
-        // The whole response has been received
-        res.on('end', () => {
-            if (res.statusCode === 200) {
-                try {
-                    let data = JSON.parse(json);
-                    // Is data available here?:
-                    console.log(data.html_url);
-                }
-                catch (err) {
-                    console.log("There was an error parsing the JSON!");
-                }
-            }
+    if (response.status !== 200) {
+        throw Error("Usuario no encontrado");
+    }
 
-            else if (res.statusCode !== 200) {
-                console.log('Current statusCode is:', res.statusCode);
-            }
-        });
-    }).on("error", (err) => {
-        console.log("There's an Error:", err);
-    });
+    return json;
 }
 
 
