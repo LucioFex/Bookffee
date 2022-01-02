81560d199b986d441e0c6339c354b2bd46394bea
LucioFex <luciano1d2esteban@gmail.com>
2022-01-02 17:20:35 -0300

    feat: Elimination of the repeated books on the "Home" route

--- helpers/booksApi.js
+++ helpers/booksApi.js
@@ -2,32 +2,35 @@
 const fetch = require("node-fetch");
 
 
-const getBooksData = async () => {
-    /* Function to get 12 books data from the Google Books API. */
-    let topics = orderTopics()
-
-    // for (topic of topics) {
-        
-    // }
-
-    let topic = topics[Math.floor(Math.random() * topics.length)];
+const getBooksData = async () => {  // Requires of a performance improvement later...
+    /* Function to get 12 books data from the Google Books API */
+    let books = []
+    const topics = orderTopics()
     const booksApi = {
         "api": "https://www.googleapis.com/books/v1/volumes?q=",
-        "config": "&maxResults=20&orderBy=relevance&:keyes&key="
-    }
-    const url = booksApi.api + topic + booksApi.config + process.env.apiKey;
-    const response = await fetch(url);
-    const json = await response.json();
-
-    if (response.status !== 200) {
-        throw Error("There was a problem getting the books");
+        "config": "&maxResults=5&orderBy=relevance&:keyes&key="
     }
 
-    return manageData(json);
+    var url, response, json
+    for (let topic of topics) {
+        url = booksApi.api + topic + booksApi.config + process.env.apiKey;
+        response = await fetch(url);
+        json = await response.json();
+
+        if (response.status !== 200) {
+            throw Error("There was a problem getting the books");
+        }
+
+        books.push(manageData(json))
+    }
+
+    return books;
 }
 
 const orderTopics = () => {
+    /* Function to select and order 12 defult books for the "Home" route */
     let ordenedTopics = [];
+    let randomTopic;
     let topics = [
         "the+idiot+fyodor", "the+little+prince", "frankestein", "van+hellsing",
         "the+lord+of+The+Rings", "the+black+Cat", "the+mistery+of+salem's+lot",
@@ -39,8 +42,11 @@ const orderTopics = () => {
         "Metro+2033"
     ];
 
-    for (let index = 0; index < 12; index++) {
-        ordenedTopics.push(topics[Math.floor(Math.random() * topics.length)]);
+    for (let index = 0; ordenedTopics.length <= 12; index++) {
+        randomTopic = topics[Math.floor(Math.random() * topics.length)];
+        if (!ordenedTopics.includes(randomTopic)) {
+            ordenedTopics.push(randomTopic);
+        }
     }
 
     console.log(ordenedTopics)
@@ -49,19 +55,14 @@ const orderTopics = () => {
 
 const manageData = (json) => {
     /* Function to manage the data from the Google Books API call */
-    let books = [];
-
     for (let book of json.items) {
-        if (books.length == 13) break;  // 13 index == 12 books
-
-        else if (  // Checks for book (with photo and description)
+        if (  // Checks for book (with photo and description)
             book.volumeInfo !== undefined &&
             book.volumeInfo.description !== undefined &&
             book.volumeInfo.imageLinks !== undefined) {
-            books.push(book.volumeInfo);
+                return book.volumeInfo;
         }
     }
-    return books
 }
 
 
