b94c72f26fbdc2d49b62e083aa4078662f0426af
LucioFex <luciano1d2esteban@gmail.com>
2022-01-22 15:48:31 -0300

    refactor: Of the public js file

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,27 +1,26 @@
 /* JS Document */
 
-const contactButton = document.getElementById("email");
-const copiedEmailMessage = document.getElementById("copiedMessage");
-
-const setUp = () => {
-    /* Function to execute before the DOM is loaded */
-    contactButton.addEventListener("click", copyEmailToClipboard, false);
-};
+const contactButton = document.getElementById('email');
+const copiedEmailMessage = document.getElementById('copiedMessage');
 
 const copyEmailToClipboard = () => {
     /* Function to copy the website's author Email to the clipboard */
 
-    copiedEmailMessage.style.opacity = "100%";
-    contactButton.removeEventListener("click", copyEmailToClipboard, false);
+    copiedEmailMessage.style.opacity = '100%';
+    contactButton.removeEventListener('click', copyEmailToClipboard, false);
 
-    setTimeout(() => {  // Time to copy the Email again
-        copiedEmailMessage.style.opacity = "0%";
-        contactButton.addEventListener("click", copyEmailToClipboard, false);
+    setTimeout(() => { // Time to copy the Email again
+        copiedEmailMessage.style.opacity = '0%';
+        contactButton.addEventListener('click', copyEmailToClipboard, false);
     }, 3500);
 
-    navigator.clipboard.writeText("luciano1d2esteban@gmail.com");
+    navigator.clipboard.writeText('luciano1d2esteban@gmail.com');
 };
 
+const setUp = () => {
+    /* Function to execute before the DOM is loaded */
+    contactButton.addEventListener('click', copyEmailToClipboard, false);
+};
 
 // Waits for the DOM to start the script
 window.addEventListener('load', setUp, false);
