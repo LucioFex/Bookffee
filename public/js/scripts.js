b2e6b40e6768ca4f5cc19c2190d49d6b304aca0e
LucioFex <luciano1d2esteban@gmail.com>
2022-02-16 18:27:58 -0300

    feat: Add of a 'Read More' button for the book's description

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,8 @@
 /* JS Document */
-
 const contactButton = document.getElementById('email');
-const copiedEmailMessage = document.getElementById('copiedMessage');
+const copiedEmailMessage = document.getElementById('copied-message');
+const descriptionButton = document.getElementById('description-btn');
+const bookDescription = document.getElementById('description');
 
 const copyEmailToClipboard = () => {
     /* Function to copy the website's author Email to the clipboard */
@@ -16,9 +17,23 @@ const copyEmailToClipboard = () => {
     navigator.clipboard.writeText('luciano1d2esteban@gmail.com');
 };
 
+const showDescription = () => {
+    /* In the book's description page, there are two types of description:
+    [The Lite]: A reduced version (Read More button shows the Full).
+    [The Full]: A complete version (Read Less button shows the Lite). */
+    const oldDescription = bookDescription.innerHTML;
+    const newDescription = descriptionButton.value;
+    bookDescription.innerHTML = newDescription;
+
+    // Now the 'Read More' button gets into 'Read Less' (or vice versa)
+    descriptionButton.textContent = 'Read Less';
+    descriptionButton.value = oldDescription;
+};
+
 const setUp = () => {
     /* Function to execute before the DOM is loaded */
     contactButton.addEventListener('click', copyEmailToClipboard, false);
+    descriptionButton.addEventListener('click', showDescription, false);
 };
 
 // Waits for the DOM to start the script
